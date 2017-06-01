70cd7cb302c72a2885c8e9bff5bceb2e91bd3045
root <readyidu>
2017-06-01 16:27:54 +0800

    删除跳转controller 用户认证

--- base-project/src/main/java/com/yidu/controller/JumpController.java
+++ base-project/src/main/java/com/yidu/controller/JumpController.java
@@ -1,18 +1,11 @@
 package com.yidu.controller;
-import com.yidu.result.DbApiResult;
 import com.yidu.service.IServiceInterface;
-import com.yidu.utils.PageData;
-import com.yidu.utils.PageDataEnum;
-import com.yidu.utils.PageDataInter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
-
 /**
  * Created by Administrator on 2016/10/20.
  */
@@ -20,24 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 public class JumpController {
     @Autowired(required = false)
     IServiceInterface userColService;
-    @RequestMapping(value = "/index", method = {RequestMethod.GET,RequestMethod.POST})
-    public ModelAndView index(HttpServletRequest request){
-        PageDataInter pageDataAll=new PageData(request, PageDataEnum.DEFAULT_ENUM_PARAM);
-        String token= (String) pageDataAll.get("token");
-//        String appName= (String) pageDataAll.get("appName");
-        ModelAndView mv=new ModelAndView("index");
-        if(StringUtils.isEmpty(token)){
-            mv=new ModelAndView("login");
-        }else{
-            pageDataAll.put("appname","znxy");
-            DbApiResult dbApiResult= (DbApiResult) userColService.execute("vaildLogin",pageDataAll,null);
-            boolean flag= (boolean) dbApiResult.getData();
-            if(!flag){
-                mv=new ModelAndView("login");
-            }
-        }
-        return mv;
-    }
 
     @RequestMapping(value = "/login", method = {RequestMethod.GET,RequestMethod.POST})
     public ModelAndView login(HttpServletRequest request){
